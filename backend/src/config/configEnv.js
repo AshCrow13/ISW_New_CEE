a4005e68d92c7567f3de71c9bc23662092629b9c
Francisco Catrileo Retamal <francisco.catrileo2001@alumnos.ubiobio.cl>
2025-05-25 12:26:47 -0400

    Update configEnv.js

--- backend/src/config/configEnv.js
+++ backend/src/config/configEnv.js
@@ -11,10 +11,10 @@ const envFilePath = path.resolve(_dirname, ".env");
 
 dotenv.config({ path: envFilePath });
 
-export const PORT = localhost;
-export const HOST = 5432;
-export const DB_USERNAME = Pancho;
-export const PASSWORD = cee123;
-export const DATABASE = Nuevo_CEE;
+export const PORT = process.env.PORT || 3000;
+export const HOST = process.env.HOST || "localhost";
+export const DB_USERNAME = process.env.DB_USERNAME || "root";
+export const PASSWORD = process.env.PASSWORD || "password";
+export const DATABASE = process.env.DATABASE || "mydatabase";
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const cookieKey = process.env.cookieKey;
