d8a981e119791e08c11ad191bfee2760636f2469
Francisco Catrileo Retamal <francisco.catrileo2001@alumnos.ubiobio.cl>
2025-05-25 20:10:49 -0400

    Nueva restrinccion en Controladores
    Se agrego una restriccion en los controladores para que solo puedan trabajar los estudiantes pertenecientes a la carrera de "Ingeniería en Computación e Informática"

--- backend/src/controllers/opciones.controller.js
+++ backend/src/controllers/opciones.controller.js
@@ -18,6 +18,9 @@ export async function postOpcion(req, res) {
         if (!req.user || req.user.rol !== "admin") {
             return handleErrorClient(res, 403, "No tienes permisos para crear una opción");
         }
+        if (req.user.carrera !== "Ingeniería en Computación e Informática") {
+            return handleErrorClient(res, 403, "Solo estudiantes de Ingeniería en Computación e Informática pueden crear opciones");
+        }
         //Verificamos que la votacion exista
         const { votacionId } = req.params;
         const { body } = req;
@@ -44,6 +47,9 @@ export async function deleteOpcion(req, res) {
         if (!req.user || req.user.rol !== "admin") {
             return handleErrorClient(res, 403, "No tienes permisos para eliminar una opción");
         }
+        if (req.user.carrera !== "Ingeniería en Computación e Informática") {
+            return handleErrorClient(res, 403, "Solo estudiantes de Ingeniería en Computación e Informática pueden eliminar opciones");
+        }
         const { votacionId, id } = req.params;
         const { error } = opcionesQueryValidation.validate({ id });
         if(error) return handleErrorClient(res, 400, error.message);
@@ -61,6 +67,9 @@ export async function deleteOpcion(req, res) {
 //Obtener una opcion en una votacion especifica
 export async function getOpcion(req, res) {
     try {
+        if(req.user.carrera !== "Ingeniería en Computación e Informática") {
+            return handleErrorClient(res, 403, "Solo estudiantes de Ingeniería en Computación e Informática pueden ver las opciones");
+        }
         const { votacionId, id } = req.query;
         const { error } = opcionesQueryValidation.validate({ id });
         if (error) return handleErrorClient(res, 400, error.message);
@@ -77,6 +86,9 @@ export async function getOpcion(req, res) {
 //Obtener todas las opciones de una votacion especifica
 export async function getOpciones(req, res) {
     try {
+        if(req.user.carrera !== "Ingeniería en Computación e Informática") {
+            return handleErrorClient(res, 403, "Solo estudiantes de Ingeniería en Computación e Informática pueden ver las opciones");
+        }
         const { votacionId } = req.params;
         const { error } = opcionesQueryValidation.validate({ votacionId });
         if (error) return handleErrorClient(res, 400, error.message);
@@ -95,6 +107,9 @@ export async function getOpciones(req, res) {
 //Actualizar una opcion en una votacion especifica
 export async function updateOpcion(req, res) {
     try {
+        if(req.user.carrera !== "Ingeniería en Computación e Informática") {
+            return handleErrorClient(res, 403, "Solo estudiantes de Ingeniería en Computación e Informática pueden actualizar opciones");
+        }
         //Verificamos que lo elimine solamente un admin
         if (!req.user || req.user.rol !== "admin") {
             return handleErrorClient(res, 403, "No tienes permisos para actualizar una opción");
