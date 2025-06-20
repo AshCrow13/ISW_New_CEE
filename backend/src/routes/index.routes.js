d75f65823339ed7cf28635acb7ae6a0096536e15
Francisco Catrileo Retamal <francisco.catrileo2001@alumnos.ubiobio.cl>
2025-06-19 22:01:42 -0400

    Feedback
    Se creo:
    -Controlador de Feedback
    -Rutas de Feedback
    -Middleware de Carreras
    Se Modifico:
    -Index de rutas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -8,6 +8,7 @@ import asistenciaRoutes from "./asistencia.routes.js";
 import opcionesRoutes from "./opciones.routes.js";
 import votacionRoutes from "./votacion.routes.js";
 import votosRoutes from "./votos.routes.js";
+import feedbackRoutes from "./vfeedback.routes.js";
 import instanciaRoutes from "./instancias.routes.js";
 
 
@@ -21,6 +22,7 @@ router.use("/asistencia", asistenciaRoutes);
 router.use("/votacion", votacionRoutes);
 router.use("/opcion", opcionesRoutes);
 router.use("/votar", votosRoutes);
+router.use("/feedback", feedbackRoutes);
 router.use("/asamblea", instanciaRoutes);
 
 export default router;
