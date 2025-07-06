93611e6e120cf7d3b60d2f50ab8130ab6b146b5d
Francisco Catrileo Retamal <francisco.catrileo2001@alumnos.ubiobio.cl>
2025-07-06 02:27:39 -0400

    Arreglo de errores

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 export const AuthContext = createContext();
 
 // eslint-disable-next-line react-refresh/only-export-components
-const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }) {
     const navigate = useNavigate();
